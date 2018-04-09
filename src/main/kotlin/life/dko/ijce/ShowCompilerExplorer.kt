f9dd950fdfb5ae9a49a79951a7f3b53f61fbe436
Dmitry Kozhevnikov <dmitry.kozhevnikov@jetbrains.com>
2018-04-10 00:34:28 +0300

    Create a read-only editor for assembly

--- src/main/kotlin/life/dko/ijce/ShowCompilerExplorer.kt
+++ src/main/kotlin/life/dko/ijce/ShowCompilerExplorer.kt
@@ -1,17 +1,31 @@
 package life.dko.ijce
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.openapi.fileTypes.PlainTextFileType
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.testFramework.LightVirtualFile
+
+val LOG = Logger.getInstance("life.dko.ijce.ShowCompilerExplorer")
 
 class ShowCompilerExplorer : DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        val editor = e.getData(CommonDataKeys.EDITOR) ?: return
         val project = e.project ?: return
 
-        WriteCommandAction.runWriteCommandAction(project, {
-            editor.document.insertString(0, "Godbolted!")
-        })
+        val virtualFile = LightVirtualFile("CE assembly", PlainTextFileType.INSTANCE, "mov eax, 1")
+        val fileEditorManager = FileEditorManager.getInstance(project)
+
+        val openedEditor = fileEditorManager.openTextEditor(OpenFileDescriptor(project, virtualFile), true)
+
+        if (openedEditor !is EditorEx) {
+            LOG.error("Opened editor is not EditorEx")
+            return
+        }
+
+        openedEditor.isViewer = true
+
     }
 }
